62f68bbacf3dc2cf45d3c323fd5c2a9566444210
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-02-11 19:58:24 +0100

    Bugfix

--- lib/sarrus.f95
+++ lib/sarrus.f95
@@ -147,10 +147,6 @@
 !> This implementation takes care about `INTEGER` matrices which are indexed as
 !> 2D arrays.
 !>
-!> The matrix is assumed to be defined as follows in terms of GNU Octave:  `[ 0
-!> 3  6 ; 1  4  7 ; 2  5  8 ]`.  Thereby, the given number is considered the
-!> offset to be added to `lower`.
-!>
 !> In case that the given field should not provide nine elements, zero would be
 !> returned instead.
 !!
